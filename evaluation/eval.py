9a4802e2343ac936f059728be98da8c75480a2d1
Vignesh Raja <vignesh@vigneshraja.com>
2015-12-03 15:08:17 -0600

    Add support for advanced stats and custom features

--- evaluation/eval.py
+++ evaluation/eval.py
@@ -14,6 +14,12 @@ class Evaluation:
     with open('../data/seasonStats.json') as stats:    
       self.stats = json.load(stats)
     self.year = 2015
+    self.per_game_features = ["PTS","TRB","AST","STL","BLK","FG","FT","FGA","FTA","TOV"]
+    self.advanced_features = ["USG%","TS%","PER"]
+    self.custom_features = ["INJ_TREND","PTS_TREND","RB_TREND","AST_TREND","STL_TREND","BLK_TREND", "PT_TREND"]
+    # a way to map from custom features to the functions that compute them
+    self.custom_feature_to_func = {}
+    self.initialize_custom_feature_dict()
     self.construct_data_sets()
     self.evaluate_avg_player_eff_error()
     self.evaluate_avg_player_ranking_error()
@@ -31,11 +37,21 @@ class Evaluation:
           if player_name in self.stats[next_year]:
             feat_vec = []
             per_game_stats = stat_cats["per_game"]
+            advanced_stats = stat_cats["advanced"]
             next_year_per_game_stats = next_yr_stats[player_name]["per_game"]
             eff = 0.0
             for stat_name, stat_val in per_game_stats.iteritems():
-              if stat_name in ["PTS","TRB","AST","STL","BLK","FG","FT","FGA","FTA","TOV"]:
+              if stat_name in self.per_game_features:
                 feat_vec.append(float(stat_val))
+            empty_stat = False
+            for stat_name, stat_val in advanced_stats.iteritems():
+              if stat_name in self.advanced_features:
+                if stat_val == "":
+                  empty_stat = True
+                  break
+                feat_vec.append(float(stat_val))
+            if empty_stat:
+              continue
             for stat_name, stat_val in next_year_per_game_stats.iteritems():
               if stat_name in ["PTS","TRB","AST","STL","BLK","FG","FT"]:
                 eff += float(stat_val)
@@ -45,23 +61,38 @@ class Evaluation:
             self.eff_vec.append(eff)
             self.feature_matrix.append(feat_vec)
             self.year_vec.append(int(year))
+  
+  def initialize_custom_feature_dict(self):
+    self.custom_features = ["INJ_TREND","PTS_TREND","RB_TREND","AST_TREND","STL_TREND","BLK_TREND", "PT_TREND"]
+    for feature in self.custom_features:
+      if feature.find("TREND") != -1:
+        self.custom_feature_to_func[feature] = self.compute_trend
+
+  def compute_trend(self,data_over_time):
+    # TODO: implement
+    pass
 
   def evaluate_avg_player_eff_error(self):
     # train on data from all years, but use k-fold cross validation
     np_eff_vec = np.array(self.eff_vec)
     np_feature_matrix = np.array(self.feature_matrix)
-    kf = KFold(len(self.eff_vec),n_folds=10)
+    k = 10
+    kf = KFold(len(self.eff_vec),n_folds=k)
     fold_num = 0
+    avg_fold_error = 0.0
     for train_idx, test_idx in kf:
       feat_train, feat_test = np_feature_matrix[train_idx], np_feature_matrix[test_idx]
       eff_train, eff_test = np_eff_vec[train_idx], np_eff_vec[test_idx]
       lr_model = LinearReg(feat_train, eff_train)
       avg_error = self.evaluate_model(lr_model, feat_test, eff_test)
       print "Fold {}: {} % error".format(fold_num,avg_error)
+      avg_fold_error += avg_error 
       fold_num += 1
-  
+    print "Average Error Across Folds = {}".format(avg_fold_error/k)
+
   def evaluate_avg_player_ranking_error(self):
     years = [1985+i for i in range(30)]
+    avg_rank_error = 0.0
     for exclude_year in years:
       feat_train = []
       eff_train = []
@@ -77,8 +108,10 @@ class Evaluation:
           eff_test.append(self.eff_vec[i])
           test_player_names.append(self.name_vec[i])
       lr_model = LinearReg(feat_train, eff_train)
-      avg_rank_error = self.evaluate_rank(lr_model, feat_test, eff_test, test_player_names)
-      print "Year {}: Rank Error = {}".format(exclude_year, avg_rank_error)
+      rank_error = self.evaluate_rank(lr_model, feat_test, eff_test, test_player_names)
+      avg_rank_error += rank_error
+      print "Year {}: Rank Error = {}".format(exclude_year, rank_error)
+    print "Avg Rank Error = {}".format(avg_rank_error/len(years))
 
   def evaluate_rank(self, model, features, eff, player_names):
     pred_tups = []
