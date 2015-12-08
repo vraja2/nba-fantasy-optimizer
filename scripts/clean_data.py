ea4d4b6f490d42711c31e43220939241bc4afa32
James Chen <jameschen990@gmail.com>
2015-12-07 21:45:10 -0600

    clean data

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -35,4 +35,4 @@ df['next-eff'] = [get_next_efficiency(row['player'], row['year'], df) for index,
 df = df[[not math.isnan(x) for x in df['next-eff']]]
 
 # write to file'
-df.to_csv('../data/cleanedSeasonStats.csv')
+df.to_csv('../data/cleanedSeasonStats.csv', index=False)
